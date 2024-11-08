8599eeba62ff0b4a0f9099bbbd5e376e5d051823
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2024-11-08 10:57:52 +0300

    Create USBCopy.bat

---
+++ USBCopy/USBCopy.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001
+cls
+setlocal
+
+set flash_drive=E:\
+
+if not exist "%flash_drive%" (
+    echo Flash drive not found.
+    exit /b
+)
+
+echo Copying files to a flash drive...
+xcopy "D:\example.txt\*.*" "%flash_drive%" /s /i /y
+
+echo Copying completed.
+pause
